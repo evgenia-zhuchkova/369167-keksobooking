bd0589ae2932f02a447ddbb1e1123e33f1dc181d
Evgenia Zhuchkova <jan-k@mail.ru>
2018-04-23 16:26:41 +0300

    Исправляет пробел в строчке 64

--- js/map.js
+++ js/map.js
@@ -61,7 +61,7 @@ var appartmentPrice = {
   'bungalo': 0,
   'flat': 1000,
   'house': 5000,
-  'palace': 10000  
+  'palace': 10000
 };
 var selectRooms = activeForm.querySelector('[name="rooms"]');
 var selectPlace = activeForm.querySelector('[name="capacity"]');
