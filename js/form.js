f3a27aa8d534a03d24d700f5a727429eeaa582b4
Evgenia Zhuchkova <jan-k@mail.ru>
2018-05-05 16:40:09 +0300

    Исправляет замечания v2

--- js/form.js
+++ js/form.js
@@ -14,30 +14,30 @@
   var form = document.querySelector('.ad-form');
   var successMsg = document.querySelector('.success');
   var valid = true;
-  
+
   var inputFocusHandler = function (evt) {
     valid = true;
     if (evt.target.style.border) {
       evt.target.style.border = '';
       var box = evt.target.parentElement.querySelector('.error-message');
-      evt.target.parentElement.removeChild(box); 
-    } 
+      evt.target.parentElement.removeChild(box);
+    }
   };
 
   var inputBlurHandler = function (evt) {
     evt.target.removeEventListener('focus', inputFocusHandler);
     evt.target.removeEventListener('blur', inputBlurHandler);
   };
-  
+
   var showError = function (elem, message) {
-      elem.style.border = '1px solid #D8000C';
-      var box = document.createElement('p');
-      box.classList.add('error-message');
-      box.textContent = message;
-      elem.parentElement.appendChild(box);
-      elem.addEventListener('focus', inputFocusHandler);
-      elem.addEventListener('blur', inputBlurHandler);
-    };
+    elem.style.border = '1px solid #D8000C';
+    var box = document.createElement('p');
+    box.classList.add('error-message');
+    box.textContent = message;
+    elem.parentElement.appendChild(box);
+    elem.addEventListener('focus', inputFocusHandler);
+    elem.addEventListener('blur', inputBlurHandler);
+  };
 
   var formSubmitHandler = function (evt) {
     evt.preventDefault();
